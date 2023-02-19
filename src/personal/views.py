74e288f4e7965ca21b031402c3494a8b4b5d564c
Nipesh Maharjan <88616145+nipu-pasa@users.noreply.github.com>
2023-02-19 09:50:06 +0530

    django models

--- src/personal/views.py
+++ src/personal/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from personal.models import Question
 
 # Create your views here.
 def home_screen_view(request):
     context = {}
-    context['some_string']="this is the some string i am passing to the view"
-    context['some_number'] = 123223
+    # context['some_string']="this is the some string i am passing to the view"
+    # context['some_number'] = 123223
+    questions=Question.objects.all()
+    context['questions']=questions
     return render(request,"personal/home.html",{})
