dc9b5a14521a5a9c66e13c5838bbe45e1bbb6f71
Nipesh Maharjan <88616145+nipu-pasa@users.noreply.github.com>
2023-02-19 12:11:16 +0530

    logging in users

--- src/account/views.py
+++ src/account/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
-from account.forms import RegistrationForm
+from django.contrib.auth import login, authenticate,logout
+from account.forms import RegistrationForm,AccountAuthenticationForm
 # from blog.models import BlogPost
 def registration_view(request):
 	context = {}
@@ -20,3 +20,38 @@ def registration_view(request):
 		form = RegistrationForm()
 		context['registration_form'] = form
 	return render(request, 'account/register.html', context)
+
+
+
+def logout_view(request):
+	logout(request)
+	return redirect('/')
+
+
+
+def login_view(request):
+
+	context = {}
+
+	user = request.user
+	if user.is_authenticated:
+		return redirect("home")
+
+	if request.POST:
+		form = AccountAuthenticationForm(request.POST)
+		if form.is_valid():
+			email = request.POST['email']
+			password = request.POST['password']
+			user = authenticate(email=email, password=password)
+
+			if user:
+				login(request, user)
+				return redirect("home")
+
+	else:
+		form = AccountAuthenticationForm()
+
+	context['login_form'] = form
+
+	# print(form)
+	return render(request, "account/login.html", context)
