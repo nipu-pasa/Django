8bb1bea86b1ea56622066fe81b4ff69ffd62f8d3
Nipesh Maharjan <88616145+nipu-pasa@users.noreply.github.com>
2023-02-21 17:41:22 +0530

    setup blog search bar

--- src/personal/views.py
+++ src/personal/views.py
@@ -1,34 +1,34 @@
 from django.shortcuts import render
-# from personal.models import Question
-# from account.models import Account
-from blog.models import BlogPost
-from blog.views import get_blog_queryset
-from django.core.pagination import EmptyPage,PageNoteAnInteger,Paginator
 from operator import attrgetter
-# Create your views here.
-BLOG_POSTS_PER_PAGE=1
-def home_screen_view(request):
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
+from blog.views import get_blog_queryset
+from blog.models import BlogPost
+
+BLOG_POSTS_PER_PAGE = 10
+
+
+def home_screen_view(request, *args, **kwargs):
     context = {}
-    query=""
+
+    # Search
+    query = ""
     if request.GET:
-        query=request.GET.get('q','')
-        context['query']=str(query)
-    # accounts=Account.objects.all()
-    # # context['some_string']="this is the some string i am passing to the view"
-    # # context['some_number'] = 123223
-    # # questions=Question.objects.all()
-    # context['accounts']=accounts
-    blog_posts=sorted(BlogPost.objects.all(),key=attrgetter('date_updated'),reverse=True )
-    context['blog_posts']=blog_posts
-    page = request.GET.get('page',1)
-    blog_posts_paginator= Paginator(blog_posts,BLOG_POSTS_PER_PAGE)
+        query = request.GET.get('q', '')
+        context['query'] = str(query)
+
+    blog_posts = sorted(get_blog_queryset(query), key=attrgetter('date_updated'), reverse=True)
+
+    # Pagination
+    page = request.GET.get('page', 1)
+    blog_posts_paginator = Paginator(blog_posts, BLOG_POSTS_PER_PAGE)
     try:
-        blog_posts =blog_posts_paginator.page(page)
-    except PageNoteAnInteger:
-        blog_posts =blog_posts_paginator.page(BLOG_POSTS_PER_PAGE)
+        blog_posts = blog_posts_paginator.page(page)
+    except PageNotAnInteger:
+        blog_posts = blog_posts_paginator.page(BLOG_POSTS_PER_PAGE)
     except EmptyPage:
-        blog_posts =blog_posts_paginator.page(blog_posts_paginator.num_pages)
-    context['blog_posts']=blog_posts
+        blog_posts = blog_posts_paginator.page(blog_posts_paginator.num_pages)
 
+    context['blog_posts'] = blog_posts
 
-    return render(request,"personal/home.html",context)
+    return render(request, "personal/home.html", context)
